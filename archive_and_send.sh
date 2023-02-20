3afb67efc9821c0c4127abb783d81c6a51c22511
jovonho <loic.hovon@gmail.com>
2023-02-20 10:31:41 -0500

    fix dlio write trace

--- archive_and_send.sh
+++ archive_and_send.sh
@@ -5,34 +5,24 @@
 
 # Archive and zip all directories matching pattern
 pushd trace_results
-for d in $(ls | grep UNET_)
+for d in $(ls | grep DLIO_)
 do
     echo $d
     tar cvzf ../archive/${d}.tar.gz $d
     [ $? != 0 ] && exit 1
-    scp ../archive/${d}.tar.gz lhovon@discslab-server1:/data/lhovon/traces/UNET_original_1w_2
+    scp ../archive/${d}.tar.gz lhovon@discslab-server1:/data/lhovon/traces/DLIO_new
 done
 popd
 
-pushd trace_results
-for d in $(ls | grep -E 'DLRM_.*_32ksteps')
-do
-    echo $d
-    tar cvzf ../archive/${d}.tar.gz $d
-    [ $? != 0 ] && exit 1
-    scp ../archive/${d}.tar.gz lhovon@discslab-server1:/data/lhovon/traces/DLRM_32ksteps_2
-done
-popd
-
-pushd trace_results
-for d in $(ls | grep -E 'BERT_horovod')
-do
-    echo $d
-    tar cvzf ../archive/${d}.tar.gz $d
-    [ $? != 0 ] && exit 1
-    scp ../archive/${d}.tar.gz lhovon@discslab-server1:/data/lhovon/traces/BERT_horovod_2
-done
-popd
+# pushd trace_results
+# for d in $(ls | grep -E 'BERT_horovod')
+# do
+#     echo $d
+#     tar cvzf ../archive/${d}.tar.gz $d
+#     [ $? != 0 ] && exit 1
+#     scp ../archive/${d}.tar.gz lhovon@discslab-server1:/data/lhovon/traces/BERT_horovod_2
+# done
+# popd
 
 
 # pushd trace_results
--- run_experiments.sh
+++ run_experiments.sh
@@ -12,6 +12,7 @@ fi
 
 
 declare -a num_gpus=(8 6 4 2)
+declare -a num_workers=(1 2 4 6 8)
 
 
 # # per worker batch sizes
@@ -24,69 +25,265 @@ UNET_OUTPUT_DIR="/raid/data/imseg/run_output"
 DLRM_OUTPUT_DIR="/raid/data/dlrm/run_output"
 BERT_OUTPUT_DIR="/raid/data/bert/run_output"
 
+DLIO_OUTPUT_DIR="/raid/data/dlio/run_output"
+
 rm -f experiments_run
 touch experiments_run
 
 
-for num_gpu in "${num_gpus[@]}";
-do  
-    for batch_size in "${batch_sizes_dlrm[@]}"
-    do
-        exp_name="DLRM_${num_gpu}GPU_batch${batch_size}_32ksteps"
-
-        if [ $DRY_RUN = true ]
-        then
-            echo $exp_name
-        else
-            # This DLRM run should be relatively long, eval twice at the middle and at the end
-            echo $exp_name | tee -a experiments_run
-            ./trace_v2.sh -w dlrm -l /dl-bench/lhovon/mlcomns_dlrm/start_training.sh -c dlrm_loic -n $num_gpu -e $exp_name -- dlrm:loic $batch_size
-            mv ${DLRM_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/app.log
-            mv ${DLRM_OUTPUT_DIR}/dlrm_tera.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/dlrm.log
-            rm -rf ${DLRM_OUTPUT_DIR}/*
-        fi
-    done
-done
+# BATCH_SIZE=${4:-2048}
+# BATCH_SIZE_EVAL=${5:-16384}
+# NUM_BATCHES=${6:-32768}
+# NUM_BATCHES_EVAL=${7:-2048}
+# NUM_WORKERS=${8:-0}
 
 
-for num_gpu in "${num_gpus[@]}";
-do  
-    for batch_size in "${batch_sizes_unet[@]}"
-    do
-        exp_name="UNET_original_1w_${num_gpu}GPU_batch${batch_size}"
+# DLRM - Investigate why every 30sec VFS reads latency spikes up leading to loss of GPU activity
 
-        if [ $DRY_RUN = true ]
-        then
-            echo $exp_name
-        else
-            echo $exp_name | tee -a experiments_run
-            # UNET - by default it will do 1 worker and do step 7
-            ./trace_v2.sh -w imseg -l /dl-bench/lhovon/mlcomns_imseg/start_training.sh -c unet3d_loic -n $num_gpu -e $exp_name -- $batch_size
-            mv ${UNET_OUTPUT_DIR}/unet3d.log /dl-bench/lhovon/tracing_tools/trace_results/$exp_name
-            rm -rf ${UNET_OUTPUT_DIR}/*
-        fi
-    done
-done
+rm -rf ${DLRM_OUTPUT_DIR}/*
+
+# Potentially generat emore insight!!!
+exp_name="DLRM_8g2kb0w_eval8k_bc_no_drop"
+echo $exp_name | tee -a experiments_run
+./trace_v2.sh -w dlrm -l /dl-bench/lhovon/mlcomns_dlrm/start_training.sh -c dlrm_loic -n 8 -e $exp_name -s -- dlrm:eval-every-2k 2048 16384 16384 8096
+mv ${DLRM_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/app.log
+mv ${DLRM_OUTPUT_DIR}/dlrm_tera.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/dlrm.log
+rm -rf ${DLRM_OUTPUT_DIR}/*
 
 
+exp_name="DLRM_8g2kb0w_print-less"
+echo $exp_name | tee -a experiments_run
+./trace_v2.sh -w dlrm -l /dl-bench/lhovon/mlcomns_dlrm/start_training.sh -c dlrm_loic -n 8 -e $exp_name -s -- dlrm:print-less 2048 16384 2048 
+mv ${DLRM_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/app.log
+mv ${DLRM_OUTPUT_DIR}/dlrm_tera.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/dlrm.log
+rm -rf ${DLRM_OUTPUT_DIR}/*
 
-for num_gpu in "${num_gpus[@]}";
-do  
-    for batch_size in "${batch_sizes_bert[@]}"
-    do
-        exp_name="BERT_horovod_${num_gpu}GPU_batch${batch_size}_2400s"
+exp_name="DLRM_8g2kb0w_no-tensorboard"
+echo $exp_name | tee -a experiments_run
+./trace_v2.sh -w dlrm -l /dl-bench/lhovon/mlcomns_dlrm/start_training.sh -c dlrm_loic -n 8 -e $exp_name -s -- dlrm:no-tb 2048 16384 2048 
+mv ${DLRM_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/app.log
+mv ${DLRM_OUTPUT_DIR}/dlrm_tera.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/dlrm.log
+rm -rf ${DLRM_OUTPUT_DIR}/*
 
-        if [ $DRY_RUN = true ]
-        then
-            echo $exp_name
-        else
-            echo $exp_name | tee -a experiments_run
-            ./trace_v2.sh -w bert -l /dl-bench/lhovon/mlcomns_bert/start_training.sh -c bert_loic -n $num_gpu -e $exp_name -- $batch_size bert:loic
-            mv ${BERT_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/bert.log
-            rm -rf ${BERT_OUTPUT_DIR}/*
-        fi
-    done
-done
+
+# First noshuffle some traces looked shifted to the right??
+exp_name="DLRM_8GPU_batch2048_0w_no_shuffle_2"
+echo $exp_name | tee -a experiments_run
+./trace_v2.sh -w dlrm -l /dl-bench/lhovon/mlcomns_dlrm/start_training.sh -c dlrm_loic -n 8 -e $exp_name -- dlrm:loic 2048 16384 32768 2048 0 "noshuffle"
+mv ${DLRM_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/app.log
+mv ${DLRM_OUTPUT_DIR}/dlrm_tera.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/dlrm.log
+rm -rf ${DLRM_OUTPUT_DIR}/*
+
+
+
+# UNET3D - With the seed bug fix, for comparison with DLIO, same config.
+exp_name="UNET_bugfix_8gb4e50_1w"
+echo $exp_name | tee -a experiments_run
+./trace_v2.sh -w imseg -l /dl-bench/lhovon/mlcomns_imseg/start_training.sh -c unet3d_loic -n 8 -e $exp_name -- 4 unet3d:loic 1
+mv ${UNET_OUTPUT_DIR}/*.log /dl-bench/lhovon/tracing_tools/trace_results/$exp_name
+rm -rf ${UNET_OUTPUT_DIR}/*
+
+
+exp_name=DLIO_UNET_8GPU_1w_b4
+./trace_v2.sh -w dlio -l /dl-bench/lhovon/dlio/start_training.sh -c dlio_loic -n 8 -e $exp_name -- unet3d
+
+log_dir=$(ls -d ${DLIO_OUTPUT_DIR}/unet3d/*/ | sort -r | head -n 1)
+mv ${log_dir}/dlio.log trace_results/${exp_name}/
+rm -r ${log_dir}/*
+
+exp_name=DLIO_BERT_8GPU_b6_2400s
+./trace_v2.sh -w dlio -l /dl-bench/lhovon/dlio/start_training.sh -c dlio_loic -n 8 -e $exp_name -- bert
+
+log_dir=$(ls -d ${DLIO_OUTPUT_DIR}/bert/*/ | sort -r | head -n 1)
+mv ${log_dir}/dlio.log trace_results/${exp_name}/dlio.log
+rm -r ${log_dir}/*
+
+log_dir=$(ls -d ${DLIO_OUTPUT_DIR}/bert_eval/*/ | sort -r | head -n 1)
+mv ${log_dir}/dlio.log trace_results/${exp_name}/dlio_eval.log
+rm -r ${log_dir}/*
+
+
+
+
+
+
+
+
+
+
+# exp_name="BERT_original_1GPU_batch6_strace_writes_and_mmap_analysis"
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w bert -l /dl-bench/lhovon/mlcomns_bert/start_training.sh -c bert_loic -n 2 -e $exp_name -s -- 6 bert:original 1000
+# mv ${BERT_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/bert.log
+# rm -rf ${BERT_OUTPUT_DIR}/*
+
+# exp_name="BERT_original_2GPU_batch6_strace_writes_and_mmap_analysis"
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w bert -l /dl-bench/lhovon/mlcomns_bert/start_training.sh -c bert_loic -n 2 -e $exp_name -s -- 6 bert:original 1000
+# mv ${BERT_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/bert.log
+# rm -rf ${BERT_OUTPUT_DIR}/*
+
+
+# exp_name="BERT_original_8GPU_batch6_short_files"
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w bert -l /dl-bench/lhovon/mlcomns_bert/start_training_short_files.sh -c bert_loic -n 8 -e $exp_name -s -- 6 bert:original
+# mv ${BERT_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/bert.log
+# rm -rf ${BERT_OUTPUT_DIR}/*
+
+
+
+
+
+
+# #############################################
+# # DLIO experiments
+# #############################################
+
+
+
+
+# #############################################
+# # BERT experiments
+# #############################################
+
+
+# exp_name="BERT_horovod_8GPU_batch6_strace_writes_and_mmap_analysis"
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w bert -l /dl-bench/lhovon/mlcomns_bert/start_training.sh -c bert_loic -n 8 -e $exp_name -s -- 6 bert:horovod
+# mv ${BERT_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/bert.log
+# rm -rf ${BERT_OUTPUT_DIR}/*
+
+
+
+# exp_name="BERT_horovod_8GPU_batch6_short_files"
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w bert -l /dl-bench/lhovon/mlcomns_bert/start_training_short_files.sh -c bert_loic -n 8 -e $exp_name -s -- 6 bert:horovod
+# mv ${BERT_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/bert.log
+# rm -rf ${BERT_OUTPUT_DIR}/*
+
+
+
+
+
+
+# num_gpu=8
+
+# for num_worker in "${num_workers[@]}"
+# do
+#     exp_name="UNET_original_${num_gpu}GPU_batch4_${num_worker}workers"
+
+#     if [ $DRY_RUN = true ]
+#     then
+#         echo $exp_name
+#     else
+#         echo $exp_name | tee -a experiments_run
+#         ./trace_v2.sh -w imseg -l /dl-bench/lhovon/mlcomns_imseg/start_training.sh -c unet3d_loic -n $num_gpu -e $exp_name -- 4 unet3d:loic $num_worker
+#         mv ${UNET_OUTPUT_DIR}/*.log /dl-bench/lhovon/tracing_tools/trace_results/$exp_name
+#         rm -rf ${UNET_OUTPUT_DIR}/*
+#     fi
+# done
+
+
+# for num_worker in "${num_workers[@]}"
+# do
+#     exp_name="DLRM_8GPU_batch2048_${num_worker}workers"
+
+#     if [ $DRY_RUN = true ]
+#     then
+#         echo $exp_name
+#     else
+#         echo $exp_name | tee -a experiments_run
+#         ./trace_v2.sh -w dlrm -l /dl-bench/lhovon/mlcomns_dlrm/start_training.sh -c dlrm_loic -n 8 -e $exp_name -- dlrm:loic 2048 $num_worker
+#         mv ${DLRM_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/app.log
+#         mv ${DLRM_OUTPUT_DIR}/dlrm_tera.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/dlrm.log
+#         rm -rf ${DLRM_OUTPUT_DIR}/*
+#     fi
+# done
+
+# exp_name="DLRM_8GPU_batch2048_0w_no_shuffle"
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w dlrm -l /dl-bench/lhovon/mlcomns_dlrm/start_training.sh -c dlrm_loic -n 8 -e $exp_name -- dlrm:loic 2048 16384 32768 2048 0 "noshuffle"
+# mv ${DLRM_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/app.log
+# mv ${DLRM_OUTPUT_DIR}/dlrm_tera.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/dlrm.log
+# rm -rf ${DLRM_OUTPUT_DIR}/*
+
+
+
+
+# # 200GB UNET - 0.4x, 1x, 2x, 5x 
+
+# exp_name=UNET_200GB_1xmem
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w imseg -l /dl-bench/lhovon/mlcomns_imseg/start_training_200gb.sh -c unet3d_loic -n 8 -e $exp_name -- 2 200
+# mv ${UNET_OUTPUT_DIR}/*.log /dl-bench/lhovon/tracing_tools/trace_results/$exp_name
+# rm -rf ${UNET_OUTPUT_DIR}/*
+
+# exp_name=UNET_200GB_2xmem_2
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w imseg -l /dl-bench/lhovon/mlcomns_imseg/start_training_200gb.sh -c unet3d_loic -n 8 -e $exp_name -- 2 100 1
+# mv ${UNET_OUTPUT_DIR}/*.log /dl-bench/lhovon/tracing_tools/trace_results/$exp_name
+# rm -rf ${UNET_OUTPUT_DIR}/*
+
+# exp_name=UNET_200GB_5xmem_2
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w imseg -l /dl-bench/lhovon/mlcomns_imseg/start_training_200gb.sh -c unet3d_loic -n 8 -e $exp_name -- 2 40 1
+# mv ${UNET_OUTPUT_DIR}/*.log /dl-bench/lhovon/tracing_tools/trace_results/$exp_name
+# rm -rf ${UNET_OUTPUT_DIR}/*
+
+
+# # UNET 30GB synthetic dataset
+
+# exp_name=UNET_30GB_generated2
+# echo $exp_name | tee -a experiments_run
+# ./trace_v2.sh -w imseg -l /dl-bench/lhovon/mlcomns_imseg/start_training_on_generated.sh -c unet3d_loic -n 8 -e $exp_name -- 2
+# mv ${UNET_OUTPUT_DIR}/unet3d.log /dl-bench/lhovon/tracing_tools/trace_results/$exp_name
+# rm -rf ${UNET_OUTPUT_DIR}/*
+
+
+
+
+
+
+
+# for num_gpu in "${num_gpus[@]}";
+# do  
+#     for batch_size in "${batch_sizes_dlrm[@]}"
+#     do
+#         exp_name="DLRM_${num_gpu}GPU_batch${batch_size}_32ksteps_ckpt"
+
+#         if [ $DRY_RUN = true ]
+#         then
+#             echo $exp_name
+#         else
+#             # This DLRM run should be relatively long, eval twice at the middle and at the end
+#             echo $exp_name | tee -a experiments_run
+#             ./trace_v2.sh -w dlrm -l /dl-bench/lhovon/mlcomns_dlrm/start_training.sh -c dlrm_loic -n $num_gpu -e $exp_name -- dlrm:loic $batch_size
+#             mv ${DLRM_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/app.log
+#             mv ${DLRM_OUTPUT_DIR}/dlrm_tera.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/dlrm.log
+#             rm -rf ${DLRM_OUTPUT_DIR}/*
+#         fi
+#     done
+# done
+
+
+
+# for num_gpu in "${num_gpus[@]}";
+# do  
+#     for batch_size in "${batch_sizes_bert[@]}"
+#     do
+#         exp_name="BERT_horovod_${num_gpu}GPU_batch${batch_size}_2400s_eval_1500s"
+
+#         if [ $DRY_RUN = true ]
+#         then
+#             echo $exp_name
+#         else
+#             echo $exp_name | tee -a experiments_run
+#             ./trace_v2.sh -w bert -l /dl-bench/lhovon/mlcomns_bert/start_training.sh -c bert_loic -n $num_gpu -e $exp_name -- $batch_size bert:loic
+#             mv ${BERT_OUTPUT_DIR}/app.log /dl-bench/lhovon/tracing_tools/trace_results/${exp_name}/bert.log
+#             rm -rf ${BERT_OUTPUT_DIR}/*
+#         fi
+#     done
+# done
 
 
 
