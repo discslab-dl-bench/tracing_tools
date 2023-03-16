07c0df40e6dfb02791c3714b162aaeabbb117153
Zachary Doucet <zacharydoucet01@hotmail.com>
2023-03-16 15:52:59 -0400

    Refactor and add jobs (#7)
    * Rough outlined refactoring
    * Add multi jobs in the launch script
    * Cleaned up and fixed a few typos
    * Factored out bpftrace
    * Made each job push their output to their own subdirectory
    * Touch up on usage and fixed inconsistencies

--- trace_v2.sh
+++ trace_v2.sh
@@ -1,78 +1,59 @@
 #!/bin/bash
+MAX_NUM_GPUS=8
 
-# This script will launch and trace DLIO.
+workload=""
+launch_script=
+container_name=
+num_gpus=1
+num_jobs=1
+exp_name="experiment"
+output_dir="./trace_results"
+use_strace=false
+extra_args=
 
+trace_bio_pid=
+trace_read_pid=
+trace_write_pid=
+trace_create_del_pid=
+trace_openat_pid=
+trace_close_pid=
+trace_time_align_pid=
+trace_syscalls_pid=
+trace_gpu_pid=
+iostat_pid=
 
-# Helper function to terminate training and clean up behind us
-terminate_traces() {
-	# Kill the training process and the traces
-	# if using strace, it was stopped when root_pid ended
-	docker kill $1 2>/dev/null
-
-	kill $trace_bio_pid
-	kill $trace_read_pid
-	kill $trace_write_pid
-	kill $trace_create_del_pid
-	kill $trace_openat_pid
-	kill $trace_close_pid
-	kill $trace_cpu_pid
-	kill $trace_gpu_pid
-	kill $trace_syscalls_pid
-	# Need to nicely kill iostat else the json will be malformed
-	kill -SIGINT $iostat_pid
-	
-	# Kill any remaining traces that didn't get killed above
-	remaining_traces=$(ps | grep bpftrace | awk '{print $1}')
-	for proc in $remaining_traces; 
-	do	
-		kill $proc
-	done
-}
-
-usage() { 
-	echo -e "Usage: $0 [OPTIONS] [WORKLOAD ARGS]"
-	echo -e "\nOptions:"
-	echo -e "  -h, --help\t\t\tPrint this message"
-	echo -e "  -w, --workload\t\tName of the workload. Must be one of bert, dlrm, imseg, dlio or explore"
-	echo -e "                  \t\tWith explore, traces are launched without attaching to a particular workload"
-	echo -e "  -l, --launch-script\t\tPath to the workload launch script"
-	echo -e "  -c, --container\t\tName to give the docker container running the workload - defaults to train_{workload}"
-	echo -e "  -n, --num-gpus\t\tNumber of GPUs to launch the workload with, defaults to 1"
-	echo -e "  -o, --output-dir\t\tDirectory where to write the traces, defaults to ./trace_results"
-	echo -e "  -e, --experiment-name\t\tOptional experiment name, defaults to \"experiment\""
-	echo -e "  -s, --strace\t\t\tUse strace to record all system calls made (very intensive)"
-	echo -e "\nWorkload args:"
-	echo -e "  Any extra arguments passed after the above options (or after '--') will be passed as is to the workload launch script."
-	echo ""
-	exit 1
-}
+jobs_pids=()
 
 main() {
+	checkPermissionAndExit
+	getVars	$@
+	validateArguments
+	setupVars
+	clearCache
+	flushSystem
+	startAllTmuxes
+	launchAllTraces
+	launchTraining
+	performStrace
+	waitAllTrainingFinished
+	flushSystem
+	terminateTraces $container_name
+	echo "All done. Don't forget to copy the application log if you need it for plotting."
+	exit 0
+}
 
+checkPermissionAndExit() {
 	if [ "${EUID:-$(id -u)}" -ne 0 ]
 	then
 		echo "Run script as root"
 		exit -1
 	fi
+}
 
-	# See https://stackoverflow.com/questions/402377/using-getopts-to-process-long-and-short-command-line-options
-
-	TEMP=$(getopt -o hl:n:o:e:sw:c: --long help,launch-script:,num-gpus:,output-dir:,experiment-name:,strace,workload:,container: \
-				-n 'trace_v2' -- "$@")
-
+getVars() {
+	TEMP=$(getopt -o hl:n:j:o:e:sw:c: --long help,launch-script:,num-gpus:,num-jobs:,output-dir:,experiment-name:,strace,workload:,container: -n 'trace_v2' -- "$@")
 	if [ $? != 0 ] ; then usage; exit 1 ; fi
-
-	# Note the quotes around '$TEMP': they are essential!
 	eval set -- "$TEMP"
-
-	# Default values
-	workload=""
-	launch_script=
-	container_name=
-	num_gpus=1
-	exp_name="experiment"
-	output_dir="./trace_results"
-	use_strace=false
 
 	while true; do
 	case "$1" in
@@ -81,6 +62,7 @@ main() {
 		-l | --launch-script ) launch_script="$2"; shift 2 ;;
 		-c | --container ) container_name="$2"; shift 2 ;;
 		-n | --num-gpus ) num_gpus="$2"; shift 2 ;;
+		-j | --num-jobs ) num_jobs="$2"; shift 2 ;;
 		-o | --output-dir ) output_dir="$2"; shift 2 ;;
 		-e | --experiment-name ) exp_name="$2"; shift 2 ;;
 		-s | --strace ) use_strace=true; shift ;;
@@ -90,11 +72,18 @@ main() {
 	done
 
 	extra_args=$@
+}
 
-	# Check mandatory parameters were given
-	[ -z $workload ] && echo -e "Workload is mandatory!\n" && usage
+validateArguments() {
+	checkWorkloadIsOfEnumeration
+	checkLaunchScriptExist
+	checkGPUNumeric
+	checkNumJobsGpusGood
+}
 
-	# Argument validation
+checkWorkloadIsOfEnumeration() {
+	[ -z "$workload" ] && echo -e "Workload is mandatory!\n" && usage
+	
 	case $workload in
 		"bert" ) ;;
 		"dlrm" ) ;;
@@ -103,28 +92,38 @@ main() {
 		"explore" ) ;;
 		* ) echo "Error: Invalid workload given. Must be one of bert, dlrm, imseg, dlio or explore"; exit 1 ;;
 	esac
+}
 
-	[ -z $container_name ] && container_name=train_${workload}
-
-	if [ "$(docker ps -a | grep $container_name)" ]
-	then 
-		echo "Container name '$container_name' is already used by an existing container."
-		echo "Remove the container or choose a different name."
-		exit 1
-	fi
-
-	# In expore mode, the launch script is optional, but mandatory in other cases
-	if [[ $workload != "explore" ]]; then
+checkLaunchScriptExist() {
+	if [[ "$workload" != "explore" ]]; then
 		[ -z $launch_script ] && echo -e "Launch script is mandatory!\n" && usage
 	fi
-	# If given, ensure the launch script exists
+
 	[ ! -z $launch_script ] && [ ! -f $launch_script ] && echo -e "Launch script given does not exist!\n" && usage
+}
 
-	# Fix given paths i.e. remove trailing or extra slashes
+checkGPUNumeric() {
+	if ! [[ $num_gpus =~ ^[0-9]+$ ]] ; then
+		echo "Error: '$num_gpus' is not a number!"
+		usage 
+	fi
+}
+
+checkNumJobsGpusGood() {
+	if [ $(($num_gpus * $num_jobs)) -gt $MAX_NUM_GPUS ]; then
+		echo "Error: there exists not enough GPUs for the given parameters"
+		exit 1
+	fi
+}
+
+setupVars() {
+	setupOutputDirectory
+	setupContainerName
+	ensureContainerNameNotTakenErrOtherwise
+}
+
+setupOutputDirectory() {
 	output_dir=$(realpath -s  --canonicalize-missing $output_dir)
-
-	# Create the output directory
-	#exp_name="${exp_name}_$(date +'%Y%m%d%H%M%S')"
 	output_dir="${output_dir}/${exp_name}"
 
 	if [ ! -d $output_dir ] 
@@ -132,146 +131,244 @@ main() {
 		echo "Creating $output_dir"
 		mkdir -p $output_dir
 	fi
+}
 
-	# Ensure num_gpus is numeric
-	if ! [[ $num_gpus =~ ^[0-9]+$ ]] ; then
-		echo "Error: '$num_gpus' is not a number!"
-		usage 
+setupContainerName() {
+	[ -z "$container_name" ] && container_name=train_${workload}
+}
+
+ensureContainerNameNotTakenErrOtherwise() {
+	if [ "$(docker ps -a | grep $container_name)" ]
+	then 
+		echo "Container name '$container_name' is already used by an existing container."
+		echo "Remove the container or choose a different name."
+		exit 1
 	fi
+}
 
-	# Flush filesystem caches to ensure all files are read from disk
+clearCache() {
+	echo 3 > "/proc/sys/vm/drop_caches"
+}
+
+flushSystem() {
 	sync
-	echo 3 > /proc/sys/vm/drop_caches
-	sleep 2
+	waitAFewSeconds
+}
 
+startAllTmuxes() {
+	if [ $num_jobs -eq 1 ]; then 
+		startTmux $container_name
+	else
+		for ((i=0; i<$num_jobs; i++)); do
+			echo "Starting tmux ${container_name}-${i}"
+			startTmux "${container_name}-${i}"
+		done
+	fi
+}
+
+startTmux() {	
+	killPreviousTmuxFromContainerNameIfExists $1
+	startNewTmuxSessionFromContainerName $1
+}
+
+killPreviousTmuxFromContainerNameIfExists() {
+	tmux kill-session -t $1 2>/dev/null
+}
+
+startNewTmuxSessionFromContainerName() {
+	tmux new-session -d -s $1
+}
+
+launchAllTraces() {
 	echo "Starting traces"
-	# Kill the tmux session from a previous run if it exists
-	tmux kill-session -t $container_name 2>/dev/null
+	startBpfTraces
+	startGpuCpuTrace
+	startMonitoringGpuMemoryUse
+	startIOStat
+	echo "All traces launched"
+}
 
-	# Start a new tmux session from which we will run training
-	tmux new-session -d -s $container_name
+startBpfTraces() {
+  trace_bio_pid=$(launchBpfTrace "trace_bio")
+  trace_read_pid=$(launchBpfTrace "trace_read")
+  trace_write_pid=$(launchBpfTrace "trace_write")
+  trace_create_del_pid=$(launchBpfTrace "trace_create_del")
+  trace_openat_pid=$(launchBpfTrace "trace_openat")
+  trace_close_pid=$(launchBpfTrace "trace_close")
+  trace_time_align_pid=$(launchBpfTrace "trace_time_align")
+  trace_syscalls_pid=$(launchBpfTrace "syscalls")
+}
 
-	# Start the bpf traces, storing their pid
-	bpftrace traces/${workload}/trace_bio.bt -o ${output_dir}/trace_bio.out &
-	trace_bio_pid=$!
+launchBpfTrace() {
+	bpftrace "traces/${workload}/${1}.bt" -o "${output_dir}/${1}.out" &> /dev/null &
+	echo $!
+}
 
-	bpftrace traces/${workload}/trace_read.bt -o ${output_dir}/trace_read.out &
-	trace_read_pid=$!
-
-	bpftrace traces/${workload}/trace_write.bt -o ${output_dir}/trace_write.out &
-	trace_write_pid=$!
-
-	bpftrace traces/${workload}/trace_create_del.bt -o ${output_dir}/trace_create_del.out &
-	trace_create_del_pid=$!
-
-	bpftrace traces/${workload}/trace_openat.bt -o ${output_dir}/trace_openat.out &
-	trace_openat_pid=$!
-
-	bpftrace traces/${workload}/trace_close.bt -o ${output_dir}/trace_close.out &
-	trace_close_pid=$!
-
-	# Start time alignment trace
-	bpftrace traces/trace_time_align.bt -o ${output_dir}/trace_time_align.out &
-	trace_time_align_pid=$!
-
-	bpftrace traces/syscalls.bt -o ${output_dir}/syscalls.out &
-	trace_syscalls_pid=$!
-
-	# Start the CPU and GPU traces
+startGpuCpuTrace() {
 	mpstat 1 > ${output_dir}/cpu.out &
 	trace_cpu_pid=$!
+}
 
-	#TODO: Explore using Nsight for GPU tracing
+startMonitoringGpuMemoryUse() {
 	nvidia-smi pmon -s um -o DT -f ${output_dir}/gpu.out &		
 	trace_gpu_pid=$!
+}
 
-	# NEW iostat
+startIOStat() {
 	iostat -mdxty sda sdb 1 -o JSON	> ${output_dir}/iostat.json &
 	iostat_pid=$!
+}
 
-	echo "All traces launched"
-
-	# Launch training in a tmux session, and wait until completion. 
-	# At this point, if we don't have a launch script, we are in pure exploration mode
-	# which is handled in the else clause: we just launch the traces and wait for Ctrl-C
+launchTraining() {
 	if [[ ! -z $launch_script ]]; then
-		echo "Starting training with command: ${launch_script} $num_gpus $container_name ${extra_args}"
-		# Start training within the tmux session, passing any extra arguments
-		tmux send-keys -t $container_name "${launch_script} $num_gpus $container_name ${extra_args}" C-m
-		sleep 1
-		# Get the system-wide PID of the root process ID in the container (bash)
-		root_pid=$(docker inspect -f '{{.State.Pid}}' $container_name)
+		launchAsWorkload
+	else
+		launchAsExploration
+	fi
+}
 
-		max_retries=100
-		# If the previous command did not work (sometimes we must wait a bit), retry in a loop
-		while [ -z "$root_pid" ]
-		do
-			if [ $max_retries == 0 ]; then
-				echo "ERROR: Could not get root PID. Exiting."
-				terminate_traces $container_name
-				exit 1
-			fi
-			max_retries=$(( $max_retries-1 ))
-			sleep 0.05
-			root_pid=$(docker inspect -f '{{.State.Pid}}' $container_name) 2>/dev/null
+launchAsWorkload() {
+	if [ $num_jobs -eq 1 ]; then 
+		launchJob $container_name $launch_script "-o ${output_dir}"
+	else 
+		for ((i=0; i<$num_jobs; i++)); do 
+			mkdir "${output_dir}/job${i}"
+			launchJob "$container_name-$i" $launch_script "-x $(getGpuNum $i) -o ${output_dir}/job${i}"
 		done
+	fi
+}
 
-		echo "root pid: \"$root_pid\""
+getGpuNum() {
+	output=""
+	for ((i=0; i<$num_gpus; i++)); do
+		output+="$(($i + $num_gpus * $1)),"
+	done
+	echo ${output%,}
+}
 
-		if $use_strace; then
-			# Attach the syscall trace to the root_process 
-			# It will automatically attach to all spawned child processes (-f flag)
-			strace -T -tt -f -p $root_pid -e 'trace=!ioctl,clock_gettime,clock_nanosleep,sched_yield,nanosleep,sched_getaffinity,sched_setaffinity,futex,set_robust_list,poll,epoll_wait,brk' -o ${output_dir}/strace.out &
+launchJob() {
+	echo "Starting training with command: $2 $num_gpus $1 $3 $extra_args"
+	tmux send-keys -t $1 "$2 $num_gpus $1 $3 $extra_args" C-m
+	waitAFewSeconds
+	pid=$(getPIDOfLaunchedWorkload $1)
+	jobs_pids+=($pid)
+	echo "root pid: \"$pid\""
+}
+
+getPIDOfLaunchedWorkload() {
+	maxAttempts=100
+	pid=$(getPIDOfRootOfContainer $1)
+	while [ -z "$pid" ]
+	do
+		if [ $maxAttempts == 0 ]; then
+			echo "ERROR: Could not get root PID. Exiting." > /dev/tty
+			terminate_traces $1
+			exit 1
 		fi
+		echo "Attempt: $maxAttempts, PID: $pid" > /dev/tty
+		maxAttempts=$(( $maxAttempts - 1 ))
+		sleep 0.05
+		pid=$(getPIDOfRootOfContainer $1)
+	done
+	echo $pid
+}
 
-		# # Save PID/TID map for later reference
-		# docker top $container_name -efT > ${output_dir}/pids_$(date +'%m%d%H%M%S').out
+getPIDOfRootOfContainer() {
+	pid=$(docker inspect -f '{{.State.Pid}}' $1) 2>/dev/null
+	echo $pid
+}
 
-		# Sleep a bit to let training spawn all workers
-		sleep 120 && echo "Slept 120s, collecting PIDs/TIDs again and ending time_alignment trace"
-		# Capture PIDs/TIDs again now that workload should be in steady state
-		docker top $container_name -efT > ${output_dir}/pids_$(date +'%m%d%H%M%S').out
+attachStraceToPid() {
+	strace -T -tt -f -p $1 -e 'trace=!ioctl,clock_gettime,clock_nanosleep,sched_yield,nanosleep,sched_getaffinity,sched_setaffinity,futex,set_robust_list,poll,epoll_wait,brk' -o ${output_dir}/strace.out &
+}
 
-		# Kill the time alignment trace early, 2min should be plenty
-		kill $trace_time_align_pid
+launchAsExploration() {
+	ps aux -T > ${output_dir}/pids_$(date +'%m%d%H%M%S').out
+	echo "Hit Ctrl-C to stop... the time alignment trace will stop in 60s"
+	read -t 60 -r -d '' _ </dev/tty
+	echo "60s have passed, killing time_alignment trace"
+	kill $trace_time_align_pid
+	echo "Hit Ctrl-C to stop..."
+	read -r -d '' _ </dev/tty
+	echo "Ctrl-C received. Stopping trace"
+}
 
-		echo "Now waiting until training completion"
+performStrace() {
+	if $use_strace; then
+		attachStraceToPid $root_pid_launched_workload
+	fi
+	sleep 120 && echo "Slept 120s, collecting PIDs/TIDs again and ending time_alignment trace"
+	for ((i=0; i<$num_jobs; i++)); do
+		docker top "${container_name}-${i}" -efT >> ${output_dir}/pids_$(date +'%m%d%H%M%S').out
+	done
+	
+	kill $trace_time_align_pid
+	echo "Now waiting until training completion"
+}
 
-		while kill -0 "$root_pid"; do
+waitAllTrainingFinished() {
+	for pid in "${jobs_pids[@]}"; do
+		while kill -0 "$pid"; do
 			sleep 5
 		done
-	else
-		# In exploration mode, we stop the time alignment trace after 60s 
-		# then keep running until Ctrl-C is received
-		ps aux -T > ${output_dir}/pids_$(date +'%m%d%H%M%S').out
-	
-		echo "Hit Ctrl-C to stop... the time alignment trace will stop in 60s"
+	done
+}
 
-		# idle waiting for ctrl-c from user for 60s
-		read -t 60 -r -d '' _ </dev/tty
 
-		echo "60s have passed, killing time_alignment trace"
-		# Kill the time alignment trace early, 2min should be plenty
-		kill $trace_time_align_pid
+usage() { 
+	echo -e "Usage: $0 [OPTIONS] [WORKLOAD ARGS]"
+	echo -e "\nOptions:"
+	echo -e "  -h, --help\t\t\tPrint this message"
+	echo -e "  -w, --workload\t\tName of the workload. Must be one of bert, dlrm, imseg, dlio or explore"
+	echo -e "                  \t\tWith explore, traces are launched without attaching to a particular workload"
+	echo -e "  -l, --launch-script\t\tPath to the workload launch script"
+	echo -e "  -c, --container\t\tName to give the docker container running the workload - defaults to train_{workload}"
+	echo -e "  -n, --num-gpus\t\tNumber of GPUs to launch a single instance of the workload with, defaults to 1"
+	echo -e "  -j, --num-jobs\t\tNumber of instances to run the workload, dividing available GPUs evenly"
+	echo -e "  -o, --output-dir\t\tDirectory where to write the traces, defaults to ./trace_results"
+	echo -e "  -e, --experiment-name\t\tOptional experiment name, defaults to \"experiment\""
+	echo -e "  -s, --strace\t\t\tUse strace to record all system calls made (very intensive)"
+	echo -e "\nWorkload args:"
+	echo -e "  Any extra arguments passed after the above options (or after '--') will be passed as is to the workload launch script."
+	echo ""
+	exit 1
+}
 
-		echo "Hit Ctrl-C to stop..."
-
-		# idle waiting for ctrl-c from user
-		read -r -d '' _ </dev/tty
-
-		echo "Ctrl-C received. Stopping trace"
+terminateTraces() {
+	if [ $num_jobs -eq 1 ]; then 
+		docker kill $1 2>/dev/null
+	else 
+		for ((i=0; i<$num_jobs; i++)); do 
+			docker kill "${1}-${i}" 2>/dev/null
+		done
 	fi
+	killAllTraces
+	killRemainingTraces
+}
 
-	# Flush out any writes to disk
-	sync
-	# Sleep a bit more once training stops to capture flushes
+killAllTraces() {
+	kill $trace_bio_pid
+	kill $trace_read_pid
+	kill $trace_write_pid
+	kill $trace_create_del_pid
+	kill $trace_openat_pid
+	kill $trace_close_pid
+	kill $trace_cpu_pid
+	kill $trace_gpu_pid
+	kill $trace_syscalls_pid
+	kill -SIGINT $iostat_pid 	# Kill iostat nicely to keep json structured	
+}
+
+killRemainingTraces() {
+	remaining_traces=$(ps | grep bpftrace | awk '{print $1}')
+	for proc in $remaining_traces; 
+	do	
+		kill $proc
+	done
+}
+
+waitAFewSeconds() {
 	sleep 10
-
-	terminate_traces $container_name
-
-	echo "All done. Don't forget to copy the application log if you need it for plotting."
-
-	exit 0
 }
 
 main $@
